dd170a7f8ab0e3fe39988e591a3f26bade7da569
skuhlmann <samkuhlmann@gmail.com>
2023-09-26 16:52:53 -0600

    connection helper

--- src/components/ConnectTBAInstructions.tsx
+++ src/components/ConnectTBAInstructions.tsx
@@ -1,6 +1,38 @@
 import { ValidNetwork } from "@daohaus/keychain-utils";
-import { H4, Link, ParLg } from "@daohaus/ui";
+import {
+  Bold,
+  Button,
+  DataLg,
+  DataMd,
+  H4,
+  Link,
+  ParLg,
+  ParMd,
+} from "@daohaus/ui";
 import { TOKENBOUND_URL } from "../utils/constants";
+import { styled } from "styled-components";
+import { useDHConnect } from "@daohaus/connect";
+
+const StepContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+const LinkContainer = styled.div`
+  display: flex;
+  align-content: center;
+  gap: 1rem;
+`;
+
+const LinkPar = styled(ParMd)`
+  color: ${({ theme }) => theme.link.color};
+  cursor: pointer;
+`;
+
+const HelperLink = styled.div`
+  margin-top: 2rem;
+`;
 
 export const ConnectTBAInstructions = ({
   contractAddress,
@@ -11,18 +43,68 @@ export const ConnectTBAInstructions = ({
   tokenId: string;
   daoChain: ValidNetwork;
 }) => {
-  return (
-    <>
-      <H4>How to connect your Token Bound Account</H4>
-      <ParLg>Step 1: Disconnect</ParLg>
-      <ParLg>
-        Step 2: Connect again with Wallet Connect and copy the connection string
-      </ParLg>
-      <ParLg>Step 3: Visit the TBA app and connect there</ParLg>
+  const { disconnect, connectWallet } = useDHConnect();
 
-      <Link href={`${TOKENBOUND_URL[daoChain]}/${contractAddress}/${tokenId}`}>
-        TBA app
-      </Link>
-    </>
+  const handleConnectionHelper = () => {
+    disconnect();
+
+    setTimeout(() => {
+      connectWallet();
+    }, 1000);
+  };
+
+  return (
+    <StepContainer>
+      <ParLg>Follow the steps below to connect this NFT to the DAO.</ParLg>
+
+      <div>
+        <ParMd>
+          <Bold>Step 1</Bold>
+        </ParMd>
+        <LinkContainer>
+          <DataMd>
+            Open the{" "}
+            <Link
+              href={`${TOKENBOUND_URL[daoChain]}/${contractAddress}/${tokenId}`}
+            >
+              <DataMd>TokenBound</DataMd>
+            </Link>{" "}
+            app.
+          </DataMd>
+        </LinkContainer>
+      </div>
+      <div>
+        <ParMd>
+          <Bold>Step 2</Bold>
+        </ParMd>
+        <DataMd>
+          Come back to this DAO app and disconnect current wallet.
+        </DataMd>
+      </div>
+
+      <div>
+        <ParMd>
+          <Bold>Step 3</Bold>
+        </ParMd>
+        <ParMd>
+          Copy Wallet Connect Code and use on the Connect with NFT button in the
+          TokenBound app.
+        </ParMd>
+      </div>
+
+      <div>
+        <ParMd>
+          <Bold>Step 4</Bold>
+        </ParMd>
+        <ParMd>Return here to interact with the DAO as your Avatar.</ParMd>
+      </div>
+
+      <HelperLink>
+        <LinkPar onClick={handleConnectionHelper}>
+          Click to disconnect current address and open Wallet Connect for code
+          (Steps 2 and 3).
+        </LinkPar>
+      </HelperLink>
+    </StepContainer>
   );
 };
