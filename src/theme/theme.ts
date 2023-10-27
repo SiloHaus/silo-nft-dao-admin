af85da9ae7d0ac67853c9be875f0aab58eeaf46d
jekl <102255715+jekl0x@users.noreply.github.com>
2023-10-27 12:53:25 -0400

    Update theme.ts

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -13,6 +13,28 @@ const border = {
   radius: "0.4rem",
 };
 
+export const siloMono = {
+  step1: "#663333", // background
+  step2: "#FFFFFF", // text
+  step3: "hsl(0, 0%, 22%)", // 
+  step4: "hsl(0, 0%, 0%)",
+  step5: "#DF7676", // hover bg
+};
+
+export const siloPrimaryBtn = {
+  ...primaryDarkBtn,
+  solid: {
+    text: siloMono.step2,
+    bg: siloMono.step1,
+    border: siloMono.step1,
+    bgHover: siloMono.step5,
+    borderHover: siloMono.step5,
+    bgFocus: siloMono.step2,
+    borderFocus: siloMono.step1,
+    bgDisabled: siloMono.step5,
+    borderDisabled: siloMono.step3,
+  },
+
 export const siloTheme: ThemeOverrides = {
   themeName: "silo",
   ...defaultDarkTheme,
@@ -20,7 +42,7 @@ export const siloTheme: ThemeOverrides = {
   rootFontColor: secondaryDark.step12,
   primary: { ...primaryDark },
   button: {
-    primary: primaryDarkBtn,
+    primary: siloPrimaryBtn,
     secondary: secondaryDarkBtn,
     success: successDarkBtn,
     warning: warningDarkBtn,
